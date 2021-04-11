dfdf5cb0966119443be6b441fd81cd6f0c3ee315
mkpietrzyk <mkm.pietrzyk@gmail.com>
2021-04-12 00:08:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import LibraryTest from "./Components/LibraryTest";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LibraryTest/>
       </header>
     </div>
   );
---
+++ src/Components/LibraryTest.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LibraryTest = (props) => {
+  return (
+    <h1>Oliasoft UI Library Test</h1>
+  );
+};
+
+export default LibraryTest;
